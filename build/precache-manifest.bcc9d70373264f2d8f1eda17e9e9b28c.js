99803548c02a429140871c1c62678a5f55da694b
Chitransh Srivastava <chitsrivastava1809@gmail.com>
2020-09-01 12:10:56 +0530

    all-in-one build

---
+++ build/precache-manifest.bcc9d70373264f2d8f1eda17e9e9b28c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11b0c3520ba021121f3a684f239a11f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "29960c8ebe9333924426",
+    "url": "./static/css/main.18ce1a8b.chunk.css"
+  },
+  {
+    "revision": "38cb3a10ce86d9ce3b03",
+    "url": "./static/js/2.1acaf573.chunk.js"
+  },
+  {
+    "revision": "cd3a3dd8182428e234199c2a92873c81",
+    "url": "./static/js/2.1acaf573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29960c8ebe9333924426",
+    "url": "./static/js/main.62d7fd2d.chunk.js"
+  },
+  {
+    "revision": "4bce6622689f66924c80",
+    "url": "./static/js/runtime-main.c1f9ea42.js"
+  }
+]);
